0e28d4b09a907566732b4f800e6d5014827efbec
Max Experience <alan@maxexperience.com.br>
2015-09-28 23:03:29 -0300

    34343

--- www/js/index.js
+++ www/js/index.js
@@ -19,13 +19,13 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+       // this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function() { 
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -33,16 +33,16 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+       // var parentElement = document.getElementById(id);
+      //  var listeningElement = parentElement.querySelector('.listening');
+      //  var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+      //  listeningElement.setAttribute('style', 'display:none;');
+       // receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
     }
